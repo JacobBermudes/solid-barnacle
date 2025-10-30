5a0bc9cba57183d86c2584dcba27124c8e9f11be
erokhinms <erokhinms@bcs.ru>
2025-10-30 21:57:28 +0500

    Simple bot

--- main.go
+++ main.go
@@ -1,176 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"mmcvpn/account"
-	"mmcvpn/banking"
-	"mmcvpn/handlers"
-	"mmcvpn/keys"
-	"mmcvpn/msg"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type mmcMsg interface {
-	HomeMsg(username string, balance int64, tariff string, adblocker bool, active string) tgbotapi.MessageConfig
-	VpnConnectMsg(currentKeys []string) tgbotapi.MessageConfig
-	PaymentMenuMsg(username string, balance int64) tgbotapi.MessageConfig
-	HelpMenuMsg() tgbotapi.MessageConfig
-	RefererMsg(userid string) tgbotapi.MessageConfig
-	DonateMsg() tgbotapi.MessageConfig
-	ThanksMsg() tgbotapi.MessageConfig
-}
-
-var messenger mmcMsg = msg.MessageCreator{
-	BotAddress: "https://t.me/mmcvpnbot",
-}
-
-type RedisReader interface {
-	AccountInit() *account.InternalAccount
-	GetUsername() string
-	GetTariff() string
-	GetAdblocker() bool
-	GetUserID() int64
-	GetActive() string
-	GetSharedKey() []string
-	GetBalance() int64
-	AccountExist() bool
-	RefferalBonus(userid int64, sum int64) int64
-}
-
-var key_sender int64
-
 func main() {
 
-	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
-	if botToken == "" {
-		log.Fatal("TELEGRAM_BOT_TOKEN environment variable not set")
-	}
-	bot, err := tgbotapi.NewBotAPI(botToken)
+	token := os.Getenv("TG_API")
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Fatalf("Failed to create bot: %v", err)
+		log.Fatal(err)
 	}
-	bot.Debug = false
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
 
-	go banking.Bank{}.StartMakePayments()
-	go startWebServer()
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	wh, _ := tgbotapi.NewWebhookWithCert("https://www.phunkao.fun:8443/"+bot.Token, tgbotapi.FilePath("cert.pem"))
+
+	_, err = bot.Request(wh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
 
 	for update := range updates {
-
-		if update.Message == nil && update.CallbackQuery == nil {
-			continue
-		}
-
-		go func(update tgbotapi.Update) {
-
-			var accountReader RedisReader = &account.InternalAccount{
-				Userid:   update.SentFrom().ID,
-				Username: update.SentFrom().UserName,
-			}
-
-			if update.CallbackQuery != nil {
-
-				accountReader.AccountInit()
-
-				callbackHandler := handlers.CallbackHandler{
-					Data:       update.CallbackQuery.Data,
-					ChatID:     update.CallbackQuery.Message.Chat.ID,
-					CallbackID: update.CallbackQuery.ID,
-					ShowHome:   false,
-					InternalAccount: account.InternalAccount{
-						Userid:   update.SentFrom().ID,
-						Username: update.SentFrom().UserName,
-					},
-				}
-
-				bot.Send(callbackHandler.Handle())
-
-				releaseButton := tgbotapi.NewCallback(callbackHandler.CallbackID, "")
-				if callbackHandler.ShowHomePage() {
-					homeMsg := messenger.HomeMsg(accountReader.GetUsername(), accountReader.GetBalance(), accountReader.GetTariff(), accountReader.GetAdblocker(), accountReader.GetActive())
-					homeMsg.ChatID = callbackHandler.ChatID
-					bot.Send(homeMsg)
-					releaseButton.Text = "Вернулись в главное меню!"
-				}
-				bot.Request(releaseButton)
-			} else if update.Message != nil {
-
-				if key_sender == accountReader.GetUserID() {
-
-					key_sender = 0
-
-					result := keys.KeyStorage{
-						Keys: []string{
-							update.Message.Text,
-						},
-					}.AddKeyToStorage()
-
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, result)
-					bot.Send(msg)
-				}
-
-				if update.Message.IsCommand() {
-
-					refArgs := update.Message.CommandArguments()
-					if strings.HasPrefix(refArgs, "ref") {
-						refID := strings.TrimPrefix(refArgs, "ref")
-						if refID != fmt.Sprintf("%d", accountReader.GetUserID()) && !accountReader.AccountExist() {
-							msg := messenger.ThanksMsg()
-							msg.ChatID = update.Message.Chat.ID
-							bot.Send(msg)
-
-							friendID, _ := strconv.ParseInt(refID, 10, 64)
-							referralBonus := account.ReferralBonus{
-								FriendID: accountReader.GetUserID(),
-								CallerID: friendID,
-								Sum:      10,
-							}
-							msgBalance := tgbotapi.NewMessage(update.Message.Chat.ID, referralBonus.ApplyBonus())
-							bot.Send(msgBalance)
-
-						}
-
-					}
-
-					commandHandler := handlers.CommandHandler{
-						ChatID:  update.Message.Chat.ID,
-						Command: update.Message.Command(),
-						InternalAccount: account.InternalAccount{
-							Userid:   update.SentFrom().ID,
-							Username: update.SentFrom().UserName,
-						},
-					}
-
-					if update.Message.Command() == "addkey" {
-						key_sender = update.SentFrom().ID
-					}
-
-					commandHandledMsg := commandHandler.Handle()
-					bot.Send(commandHandledMsg)
-				}
-
-			}
-		}(update)
-
+		log.Printf("%+v\n", update)
 	}
-}
-
-func startWebServer() {
-	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
-		// ...реализация /auth...
-	})
-	http.HandleFunc("/keys", func(w http.ResponseWriter, r *http.Request) {
-		// ...реализация /keys...
-	})
-	log.Println("Web server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
